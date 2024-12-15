590ca618637c73fe2da6271058cd0a5742b7f2ea
kunkenshou <kunkenshou@gmail.com>
2024-12-15 15:56:48 +0900

    取得したデータをテーブル形式でブラウザに表示

--- crud/src/kakeibo-app/result.php
+++ crud/src/kakeibo-app/result.php
@@ -1,20 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="ja">
+<link rel="stylesheet" href="css/styles.css">
+<head>
+<meta charset="utf-8">
+</head>
+  <body>
+  <?php
 
-$host = "db";
-$username = "book_log";
-$password = "pass";
-$database = "book_log";
+  $host = "db";
+  $username = "book_log";
+  $password = "pass";
+  $database = "book_log";
 
-$conn = new mysqli($host, $username, $password, $database);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+  $conn = new mysqli($host, $username, $password, $database);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully<br/>";
 
-$result = $conn->query("SELECT * FROM kakeibo;");
+  $result = $conn->query("SELECT * FROM kakeibo;");
 
-while ($row = $result->fetch_row()) {
+  while ($row = $result->fetch_row()) {
 
-  printf("\n (%s)\n (%s)\n (%s)\n (%s)\n (%s)\n (%s)\n", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);
-}
+    printf("<table><tr><td>(%s)</td><td>(%s)</td><td>(%s)</td><td>(%s)</td><td>(%s)</td><td>(%s)</td></tr></table>", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);
+  }
+  ?>
+</body>
+
+</html>
